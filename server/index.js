7f8f0c8fb7025d48c2f40b1a54ba8e1802381c9c
Kinng Ddx <kinngddx@gmail.com>
2025-07-12 15:58:13 +0000

    🚀 Updated backend upload setup and cleaned routes

--- server/index.js
+++ server/index.js
@@ -160,7 +160,7 @@ app.use(cors());
 app.use(express.json());
 
 
-app.use("/api/auth", require("./routes/auth"));
+// app.use("/api/auth", require("./routes/auth"));
 app.use("/api/upload", require("./routes/upload"));
 app.use("/api/transcribe", require("./routes/transcribe"));
 
